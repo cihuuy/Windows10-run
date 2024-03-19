29746717650ebcdf71e388740f24d16f0cd4cac3
cihuuy <95659029+cihuuy@users.noreply.github.com>
2024-03-19 10:06:51 +0700

    Delete winxp.sh

--- winxp.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/MooseDev99/Linux-to-Windows-with-QEMU/raw/master/Linux_QemuWindowsXPColabNoVT-X.sh
-chmod +x Linux_QemuWindowsXPColabNoVT-X.sh
-./Linux_QemuWindowsXPColabNoVT-X.sh
