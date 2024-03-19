14b5755ec8461bc78b5440be572924901cd3e9e4
cihuuy <95659029+cihuuy@users.noreply.github.com>
2024-03-19 10:07:15 +0700

    Delete win7.sh

--- win7.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/MooseDev99/Linux-to-Windows-with-QEMU/raw/master/Linux_QemuWindows7ColabNoVT-X.sh
-chmod +x Linux_QemuWindows7ColabNoVT-X.sh
-./Linux_QemuWindows7ColabNoVT-X.sh
