b6f4174410a3496ae0b2002e77ea0cddd877211e
cihuuy <95659029+cihuuy@users.noreply.github.com>
2024-03-19 10:07:31 +0700

    Delete win10.sh

--- win10.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/MooseDev99/Linux-to-Windows-with-QEMU/raw/master/Linux_QemuWindows10ColabNoVT-X.sh
-chmod +x Linux_QemuWindows10ColabNoVT-X.sh
-./Linux_QemuWindows10ColabNoVT-X.sh
